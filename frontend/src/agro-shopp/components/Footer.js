e89696ab0f32c573d0e14f74eaaab8ff2b22c301
RadojeBozic <RadojeBozic@users.noreply.github.com>
2025-06-29 16:54:02 +0200

    Izmena cenovnika

--- frontend/src/agro-shopp/components/Footer.js
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-    return (
-      <footer className="bg-dark text-white text-center p-3 mt-4">
-        © {new Date().getFullYear()} Agro-shopp. Sva prava zadržana.
-      </footer>
-    );
-  }
